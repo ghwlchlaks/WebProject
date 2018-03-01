e67949afbb3d3c74549b39120894b0fa48debbf2
ghwlchlaks <ghwlchlaks@naver.com>
2018-03-01 22:15:55 +0900

    success - Data received from server output to table

--- front/src/components/NoticeBoard.vue
+++ front/src/components/NoticeBoard.vue
@@ -2,7 +2,7 @@
 <v-app class='noticeboard'>
   <v-content>
   <v-alert type='success' :value='true'>{{board_name}}</v-alert>
-  <b-table class='cyan' striped hover :items="items" @row-clicked="test"></b-table>
+  <b-table class='cyan' striped hover :items="items" @row-clicked="rowClick_test"></b-table>
   <b-pagination-nav class='pagination' :number-of-pages="5" :link-gen="linkGen" v-model="currentPage"></b-pagination-nav>
   </v-content>
   <v-btn dark  @click="AddContents({name:'AddBoard', params: {stateBoard:'Add'}})">Add</v-btn>
@@ -16,25 +16,7 @@ export default {
       board_name: null,
       currentPage: 1,
       baseurl: null,
-      items: [{
-        value: false,
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: '1%'
-      },
-      {
-        value: false,
-        name: 'Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: '1%'
-      }
-      ]
+      items: [{}]
     }
   },
   watch: {
@@ -45,13 +27,12 @@ export default {
     },
     currentPage (val) {
       // '$route.params.boardId overap to function called when var === 1'
-      if (val !== 1) {
-        this.getData(this.board_name, val)
-      }
+      this.getData(this.board_name, val)
     }
   },
   async mounted () {
     const boardId = this.$store.state.route.params.boardId
+    this.getData(boardId, 1)
     this.initialize(boardId)
   },
   methods: {
@@ -63,7 +44,7 @@ export default {
       }
       this.$router.push(route)
     },
-    test (record, index) {
+    rowClick_test (record, index) {
       console.log('record : ', record)
       console.log('index : ', index)
     },
@@ -77,9 +58,19 @@ export default {
       this.linkGen(1)
     },
     async getData (boardId, index) {
-      console.log('boardId ' + boardId + 'index ' + index)
-      const data = await NoticeBoardService.show(boardId, index)
-      console.log(data)
+      const boardData = await NoticeBoardService.show(boardId, index)
+      var indexBoardId = boardId + '_id'
+      var changedData = [{}]
+      for (var i = 0; i < boardData.data.length; i++) {
+        // var jsonString = JSON.stringify(boardData.data[i])  to string from json
+        // var json = JSON.parse(jsonString)  to json from string
+        changedData[i] = {
+          // 'boardId'_id index access
+          'INDEX': boardData.data[i][indexBoardId],
+          'TiTLE': boardData.data[i].title
+        }
+      }
+      this.items = changedData
     }
   }
 }
